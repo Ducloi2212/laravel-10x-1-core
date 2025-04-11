f5ba5be8fa0d7f719896a34731c82325995b790a
DDLoi <dinhducloi477@gmail.com>
2025-04-11 14:26:48 +0700

    detail

--- resources/views/crud_user/read.blade.php
+++ resources/views/crud_user/read.blade.php
@@ -1,26 +1,23 @@
 @extends('dashboard')
 
 @section('content')
-    <main class="login-form">
-        <div class="container">
-            <div class="row justify-content-center">
-                <table>
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Name</th>
-                            <th>Email</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>{{$messi->id}}</td>
-                            <td>{{$messi->name}}</td>
-                            <td>{{$messi->email}}</td>
-                        </tr>
-                    </tbody>
-                </table>
+<main class="flex-grow flex items-center justify-center">
+        <div class="border p-8 rounded shadow-md">
+            <h1 class="text-center mb-4">Màn hình chi tiết</h1>
+            <div class="mb-2">
+                <span class="font-semibold">Username: </span>
+                <span class="ml-4">{{$messi->name}}</span>
+            </div>
+            <div class="mb-4">
+                <span class="font-semibold">Email: </span>
+                <span class="ml-4">{{$messi->email}}</span>
             </div>
         </div>
     </main>
+
+    <div class="w-full border-t">
+        <div class="flex justify-center py-2">
+            <span>Nhom D</span>
+        </div>
+    </div>
 @endsection
